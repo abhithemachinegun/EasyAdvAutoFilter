a5fc2d777f7ba679650094bda8ea8d23cccf4248
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2023-09-19 09:54:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -43,7 +43,7 @@ BUTTONS0 = {}
 BUTTONS1 = {}
 BUTTONS2 = {}
 SPELL_CHECK = {}
-ADMIN_USRNM = "abhi_the_machinegun"
+ADMIN_USRNM = "titaniummovieflix"
 # ENABLE_SHORTLINK = ""
 
 @Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
