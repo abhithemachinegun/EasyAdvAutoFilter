955771eea7981739ee7cfa90e72fada3d582767b
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2024-03-19 23:26:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter2')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
